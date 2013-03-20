3168e26bb264f8387ec74221563302ba3464030f
Bharat Kunwar <b.kunwar@bristol.ac.uk>
2013-03-20 13:49:08 +0000

    Wed Mar 20 13:49:08 GMT 2013

--- query.m
+++ query.m
@@ -1,54 +1,90 @@
-% after changing the query, 
-clear;
+% after changing the query,
+% clear;
 
 t1 = 'atm';
 t2 = 'pub';
-place = 'Edinburgh';
+place = 'Cardiff';
 
 q1 = ['SELECT DISTINCT ST_X(p.way), ST_Y(p.way) '...
-          'FROM planet_osm_point AS p, '...
-          '(SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS q '...          
-          'WHERE (p.amenity=''' t1 ''' OR p.tags ? ''' t1 ''') AND ST_Intersects(p.way, q.way)'];
-        
+    'FROM planet_osm_point AS p, '...
+    '(SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS q '...
+    'WHERE (p.amenity=''' t1 ''' OR p.tags ? ''' t1 ''') AND ST_Intersects(p.way, q.way)'];
+
 q2 = ['SELECT DISTINCT ST_X(p.way), ST_Y(p.way) '...
-          'FROM planet_osm_point AS p, '...
-          '(SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS q '...
-          'WHERE (p.amenity=''' t2 ''' OR p.tags ? ''' t2 ''') AND ST_Intersects(p.way, q.way)'];
-      
+    'FROM planet_osm_point AS p, '...
+    '(SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS q '...
+    'WHERE (p.amenity=''' t2 ''' OR p.tags ? ''' t2 ''') AND ST_Intersects(p.way, q.way)'];
+
 q3 = ['SELECT ST_X((g.p).geom), ST_Y((g.p).geom) '...
-           'FROM '...
-           '(SELECT ST_DumpPoints(f.way) AS p FROM '...
-           ' (SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS f) AS g'];        
+    'FROM '...
+    '(SELECT ST_DumpPoints(f.way) AS p FROM '...
+    ' (SELECT way FROM planet_osm_polygon WHERE name = ''' place ''' ORDER BY ST_NPoints(way) DESC LIMIT 1) AS f) AS g'];
 
 
 % (g.p).path[1],(g.p).path[2], ;
 
 if ~exist('p1','var')
-    p1 = importDB(q1);
+    p1 = cell2mat(importDB(q1));
 end
 
 if ~exist('p2','var')
-    p2 = importDB(q2);
+    p2 = cell2mat(importDB(q2));
 end
 
 if ~exist('p3','var')
-    p3 = importDB(q3);
+    p3 = cell2mat(importDB(q3));
 end
 
-% p1_lon = cell2mat(p1(:,1));
-% p1_lat = cell2mat(p1(:,2));
+% longitude is (:,1)
+% latitude  is (:,2)
+
+% f1 = figure;
+% set(f1,'name',[place ' x ' t1 ' o ' t2],'numbertitle','off')
 % 
-% 
-% p2_lon = cell2mat(p2(:,1));
-% p2_lat = cell2mat(p2(:,2));
+% hold on;
+% plot(p1(:,1),p1(:,2),'x','Color','blue');
+% plot(p2(:,1),p2(:,2),'o','Color','red');
+% plot(p3(:,1),p3(:,2),'.','Color','green');
 
-p3_lon = cell2mat(p3(:,1));
-p3_lat = cell2mat(p3(:,2));
+max1 = max(p3(:,1));
+max2 = max(p3(:,2));
+min1 = min(p3(:,1));
+min2 = min(p3(:,2));
 
-f = figure;
-set(f,'name',[place ' x ' t1 ' o ' t2],'numbertitle','off')
+% delta of min and max longitudes
+d1 = max1 - min1;
+% delta of min and max latitudes of the map
+d2 = max2 - min2;
 
-hold on;
-% plot(p1_lon,p1_lat,'x');
-% plot(p2_lon,p2_lat,'o');
-plot(p3_lon,p3_lat,'.');
+% level of detail in longitudinal direction
+x1 = 50;
+% level of detail in latitudinal direction calculated as a round off of the
+% proportion of the map.
+x2 = round (x1* (d2 / d1));
+
+% size of each unit cell
+u1 = d1 / x1;
+u2 = d2 / x2;
+
+% create empty matrices with zeroes to count the number of amenities that
+% fall within the each of the grid
+a1 = zeros(x1,x2);
+a2 = zeros(x1,x2);
+
+% number of each of the amenities
+[n1,~]=size(p1);
+[n2,~]=size(p2);
+
+for i = 1:n1,
+    g1 = ceil((p1(i,1) - min1)/u1);
+    g2 = ceil((p1(i,2) - min2)/u2);
+    a1(g1,g2) = a1(g1,g2) + 1;
+end
+
+for i = 1:n1,
+    g1 = ceil((p2(i,1) - min1)/u1);
+    g2 = ceil((p2(i,2) - min2)/u2);
+    a2(g1,g2) = a2(g1,g2) + 1;
+end
+
+disp(corrcoef(a1,a2));
