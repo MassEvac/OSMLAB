8784ac541cdbc7ee9865dbfc81bb6fb41109d371
Bharat Kunwar <b.kunwar@gmail.com>
2013-06-08 15:19:01 +0100

    Sat  8 Jun 2013 15:19:00 BST

--- getSmoothPopulationGrid.m
+++ getSmoothPopulationGrid.m
@@ -1,4 +1,4 @@
-function [smoothPopulationGrid] = getSmoothPopulationGrid(place,gridSize,sigma)
+function [smoothPopulationGrid,longitude,latitude] = getSmoothPopulationGrid(place,gridSize,sigma)
 
 boundary = getBoundary(place);
 [height,width,x_lon,x_lat,u_lon,u_lat]=getGridParameters(boundary,gridSize);
@@ -13,11 +13,15 @@ for i = 1:x_lon,
     for j = 1:x_lat,
         l1 = (min_lon + i * u_lon); % Longitude
         l2 = (max_lat - j * u_lat); % Latitude
+        longitude(j,i) = l1;
+        latitude(j,i) = l2;
         ldiff = abs(population(:,2) - l2) .* abs(population(:,1) - l1);
         [~, array_position] = min(ldiff);
         % testPopulation(l1, l2, population, array_position);
         % a = [a; l1 l2 p4(array_position,3)];
         populationGrid(j,i) = round(haversine_area(l1,l2,u_lon,u_lat)/10^6 * population(array_position,3));
+        % Population in the database is in terms of person/km^2. times by
+        % area in km^2 to get the exact number of people in each square
     end
 end
 
--- populationOverlay.m
+++ populationOverlay.m
@@ -1,6 +1,13 @@
-smoothPopulationGrid = getSmoothPopulationGrid('Bristol', 250, 1);
-showPopulationGrid(smoothPopulationGrid, 'Bristol');
+[pop,lon,lat]=getSmoothPopulationGrid('Bristol',250,1);
 [AM,nodes]=getAM('Bristol');
-% gplot(AM,nodes);
-% hold on;
-imagesc(smoothPopulationGrid);
+gplot(AM,nodes,'-*');
+hold on;
+plot3k([lon(:) lat(:) log(pop(:))]);
+%imagesc(population,);
+% 1. make a list of origin/destination nodes by comparing the list
+% of latitudes and longitudes with the node list and finding the nodes
+% closest to them. origins and destination will be p
+% 2. calculate the distance from node i's->j's.
+% 2. each point represents the number of people in that proximity.
+% calculate Tij from i->j by using this value.
+% 3. 
