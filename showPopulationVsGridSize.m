3db19dea5bb4b9430a57412c849e0bf32dafd5ac
Bharat Kunwar <b.kunwar@gmail.com>
2013-10-22 17:04:38 +0100

    Function that shows box plots which describes how gridSize affects population accuracy for different methods of interpolation

--- showPopulationVsGridSize.m
+++ showPopulationVsGridSize.m
@@ -1,35 +1,42 @@
-tic;
-close all;
+function showPopulationVsGridSize(place, gridSizes, sigma, saveFigures)
+% Shows a box plot which describes how gridSize affects population accuracy
+%
+% INPUT:
+%           place (String) - Name of polygon areas in OpenSteetMap
+%           gridSizes(i) (Integer Array) - Array of Grid granularity in metres
+%           sigma (Integer) - Standard deviation to use for gaussian blurring
+%           saveFigures (boolean) - Optional boolean switch for saving figures
+% OUTPUT:
+%           Box plot which describes how gridSize affects population accuracy
+% EXAMPLE:
+%           showPopulationVsGridSize('London', [100:100:4000], 2, true);
 
-place = 'London';
-sigma = 2;
-% show2Amenities('fuel','hospital',place,true);hold on; plot3k(getPopulation(place));view(2);
-% figure;
+interpolationMethods = {'bicubic' 'bilinear' 'box' 'nearest'};
+averageArea = true;
 
-steps = 40;
-gridSizeInterval = 100;
+for i = 1:length(interpolationMethods)
 
-for i = 1:steps
-    pop = getPopulationGrid(place, i*gridSizeInterval, sigma,true);
-%     imagesc(pop);
-%     drawnow;
-%     pause(0.05);
-    p(i) = sum(pop(:));
+    g = length(gridSizes);
+    population = zeros(1,g);
+
+    for j = 1:g
+        pg = getPopulationGrid(place, gridSizes(j), sigma,averageArea,interpolationMethods{i});
+        population(j) = sum(pg(:));
+    end
+
+    x = gridSizes;
+    y = 100*(population/mean(population)-1);
+    e = std(y) * ones(1,g);
+
+    figure;
+    errorbar(x,y,e);
+
+    xlabel('Gridcell Size (metres)');
+    ylabel('(P/P(average)) - 1) %');
+
+    if saveFigures
+        set(gcf,'Position', [0, 0, 400, 300]);
+        set(gcf, 'Color', 'w');
+        export_fig(['./figures/point_analysis/plot-population-vs-gridSize-' interpolationMethods{i} '-sigma-' num2str(sigma) '-' place '.pdf']);
+    end
 end
-
-x = [1:steps] * gridSizeInterval;
-y = (p/mean(p)-1)*100;
-e = std(y) * ones(1,steps);
-
-figure;
-errorbar(x,y,e);
-
-xlabel('Gridcell Size (metres)');
-ylabel('(P/P(average)) - 1) %');
-
-
-set(gcf,'Position', [0, 0, 400, 300]);
-set(gcf, 'Color', 'w');
-export_fig(['./figures/point_analysis/plot-population-vs-gridSize-box-sigma-' num2str(sigma) '-' place '.pdf']);
-
-toc;
