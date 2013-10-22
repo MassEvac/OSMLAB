7fff2765973ab832be32e9dc16a9a973d492bb97
Bharat Kunwar <b.kunwar@gmail.com>
2013-10-22 17:05:25 +0100

    Function that shows the correlation between amenity count and population count

--- showAmenityPopulationScatter.m
+++ showAmenityPopulationScatter.m
@@ -1,5 +1,31 @@
-pg = getPopulationGrid('London', 500, 1);
-PW=getAmenityGrids({'fire_station'},'London',500,1,true);pw=PW{1};
+function showAmenityPopulationScatter(amenityTag,place, gridSize, sigma, saveFigures)
+% Shows the correlation between amenity count and population count
+%
+% INPUT:
+%           amenityTag (String) - Name of the amenities to consider
+%           place (String) - Name of polygon area in OpenSteetMap
+%           gridSize (Integer) - Grid granularity in metres
+%           sigma (Integer) - Standard deviation to use for gaussian blurring
+%           saveFigures (boolean) - Optional boolean switch for saving figures
+% OUTPUT:
+%           Graph of population data overlaid on top of the highway graph
+% EXAMPLE:
+%           showAmenityPopulationScatter('police','London',400,2,true)
+
+populationWeighted = false;
+
+pg = getPopulationGrid(place, gridSize, sigma);
+AG = getAmenityGrids({amenityTag},place,gridSize,sigma,populationWeighted); %Not population weighted;
+ag = AG{1};
 figure;
-scatter(pg(:),pw(:));
-corrcoef(pg,pw)
+scatter(pg(:),ag(:));
+corrcoef(pg,ag);
+
+xlabel('Population Count');
+ylabel('Amenity Count');
+
+if saveFigures
+    set(gcf,'Position', [0, 0, 800, 300]);
+    set(gcf, 'Color', 'w');
+    export_fig(['./figures/point_analysis/scatter-populationVsAmenity-' amenityTag '-' place '-' num2str(gridSize) '-' num2str(sigma) '.pdf']);
+end    
