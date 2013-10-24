b5463da99f6eaa21aa9a398d29485369cfaaf654
Bharat Kunwar <b.kunwar@gmail.com>
2013-10-24 18:22:51 +0100

    showAmenities can now take 7 time 13 different amenities before recycling the symbols

--- showAmenities.m
+++ showAmenities.m
@@ -1,4 +1,4 @@
-function show2Amenities(amenityTag1, amenityTag2, place, saveFigures)
+function showAmenities(amenityTags, place, saveFigures)
 % Returns amenity count bitmap matrix of a given place with input attributes
 %
 % INPUT:
@@ -9,33 +9,45 @@ function show2Amenities(amenityTag1, amenityTag2, place, saveFigures)
 % OUTPUT:
 %           Plot of the 2 amenities overlaid on the plot of the boundary 
 % EXAMPLE:
-%           show2Amenities('fuel','hospital','London',true)
+%           showAmenities({'fuel' 'hospital'},'London',true)
 % NOTE:
 %           The boundary is more obvious when there are more points to
 %           indicate it. Eg. The boundary for London is a lot more obvious
 %           than the boundary for Bristol so just be wary of that.
 
-if (nargin < 4)
-    saveFigures = false;
-end
-
-amenity1 = getAmenity(amenityTag1, place);
-amenity2 = getAmenity(amenityTag2, place);
 boundary = getBoundary(place);
 
 [max_lon,max_lat,min_lon,min_lat]=getBoundaryLimits(place);
 
-if saveFigures
-    figure;
-else
-    figure('units','normalized','outerposition',[0 0 1 1]);
-end
+markers = {'.' '+' 'o' '*' 'x' 's' 'd' '^' 'v' '>' '<' 'p' 'h'};
+colours = {'g' 'r' 'b' 'c' 'm' 'y' 'k'};
+
+markerNumber = 1;
+colourNumber = 1;
+
+figure;
 
 hold on;
-plot(amenity1(:,1),amenity1(:,2),'x','Color','blue');
-plot(amenity2(:,1),amenity2(:,2),'o','Color','red');
-plot(boundary(:,1),boundary(:,2),'.','Color','green');
-legend(upper(strrep(amenityTag1, '_', ' ')),upper(strrep(amenityTag2, '_', ' ')),place,'location','southeast');
+plot(boundary(:,1),boundary(:,2),[colours{colourNumber} markers{markerNumber}]);
+
+for i = 1:length(amenityTags)
+    markerNumber = markerNumber + 1;
+    colourNumber = colourNumber + 1;
+
+    if markerNumber > length(markers)
+        markerNumber = 1;
+    end
+
+    if colourNumber > length(colours)
+        colourNumber = 1;
+    end
+    
+    amenity = getAmenity(amenityTags{i}, place);
+    plot(amenity(:,1),amenity(:,2),[colours{colourNumber} markers{markerNumber}]);
+end
+
+legend([place upper(strrep(amenityTags, '_', ' '))],'location','southeast');
+
 axis([min_lon max_lon min_lat max_lat]);
 xlabel('Longitude (degrees)');
 ylabel('Latitude (degrees)');
@@ -43,5 +55,5 @@ ylabel('Latitude (degrees)');
 if saveFigures
     set(gcf,'Position', [0, 0, 800, 300]);
     set(gcf, 'Color', 'w');
-    export_fig(['./figures/point_analysis/plot-' amenityTag1 '-' amenityTag2 '-' place '.pdf']);
+    export_fig(['./figures/point_analysis/plot-' strjoin(amenityTags,'-') '-' place '.pdf']);
 end
