6fee53cdc643f837add4ba991a578bd8936bab5a
Bharat Kunwar <b.kunwar@gmail.com>
2013-11-15 16:45:22 +0000

    Minor changes

--- misc/changeUserPath.m
+++ misc/changeUserPath.m
@@ -1,6 +1,6 @@
 [~, name] = system('hostname');
 
-if (strcmp(name(1:13),'bharat-ubuntu'))
+if (strcmp(name(1:(length(name)-1)),'IT050339'))
     userpath('/home/bk12369/OSM');
 else
     userpath('/Users/bharatkunwar/OSM');
