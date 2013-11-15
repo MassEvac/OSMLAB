a276d5a4688b994d66010aabec205a08c498fd29
Bharat Kunwar <b.kunwar@gmail.com>
2013-11-15 13:29:50 +0000

    Economies of scale analysis

--- runEconomiesOfScaleAnalysis.m
+++ runEconomiesOfScaleAnalysis.m
@@ -23,54 +23,86 @@ disp([num2str(sum(~populationCount)) ' places could not be resolved.']);
 [amenityCount] = getAmenityCountByPlace(amenityTags,places);
 
 %% Do calculations
-x=log(populationCount(~~populationCount))';
-y=[x log(amenityCount(~~populationCount,:))];
+x=log(populationCount(logical(populationCount)))';
+y=[x log(amenityCount(logical(populationCount),:))];
 
-doesNotExist = [zeros(length(x),1) ~amenityCount(~~populationCount,:)];
+%%
+exists = [true(length(x),1) logical(amenityCount(logical(populationCount),:))];
 
+%%
 tags = {'symmetry' amenityTags{:}};
 
 [~,m]=size(y);
-
-legtxt = cell(m,1);
 
 markers = {'.' '+' 'o' '*' 'x' 's' 'd' '^' 'v' '>' '<' 'p' 'h'};
 colours = {'k' 'g' 'r' 'b' 'c' 'm' 'y'};
 
 markerNumber = 1;
 colourNumber = 1;
+plotNumber = 1;
+
+range = [1 (1:10)+1];
+legtxt = cell(length(range),1);
 
 figure;
 
 hold on;
 
+[~,index]=sort(sum(exists,1),'descend');
+
 for n = 1:m
-    if markerNumber > length(markers)
-        markerNumber = 1;
+    
+    thisX = x(exists(:,n));
+    thisY = y(exists(:,n),n);    
+    
+    if length(thisX) > 1
+        p = polyfit(thisX,thisY,1);
+        k(n) = p(1); % Gradient
+        n
+    else
+        k(n) = 0;
     end
-
-    if colourNumber > length(colours)
-        colourNumber = 2;
-    end
+    
+    if find(ismember(range,n))
         
-    thisX = x(~doesNotExist(:,n));
-    thisY = y(~doesNotExist(:,n),n);
-    
-    p = polyfit(thisX,thisY,1);
-    
-    k(n) = p(1); % Gradient
-    legtxt{n}=[ num2str(k(n),'%0.2f') ' = {\beta}_{' strrep(tags{n}, '_', ' ') '}'];    
+        if markerNumber > length(markers)
+            markerNumber = 1;
+        end
 
-    plot(thisX,thisY,[colours{colourNumber} markers{markerNumber}]);    
+        if colourNumber > length(colours)
+            colourNumber = 2;
+        end
+        
+        legtxt{plotNumber}=[ num2str(k(n),'%0.2f') ' = {\beta}_{' strrep(tags{n}, '_', ' ') '}'];
+
+        plot(thisX,thisY,[colours{colourNumber} markers{markerNumber}]);    
     
-    markerNumber = markerNumber + 1;
-    colourNumber = colourNumber + 1;
+        markerNumber = markerNumber + 1;
+        colourNumber = colourNumber + 1;
+        plotNumber = plotNumber + 1;
+    end
 end
 
+legend(legtxt,'location','nw');
+
+xlabel('log (Population)');
+ylabel('log (SPOIs)');
 lsline;
 
-legend(legtxt);
+%%
+texFile = 'extendedAmenity.tex';
+latextable(amenityCount,'horiz',amenityTags,'vert',places,'name',texFile,'Hline',[1],'Vline',[1])
 
-xlabel('Population');
-ylabel('SPOIs');
-lsline;
+
+%%
+[~,index]=sort(k,'descend');
+texFile = 'gradient.tex';
+latextable(k(index)','format','%0.2f','vert',strcat('$\beta_{', strrep(tags(index)', '_', '\_')','}$'),'name',texFile,'Hline',[1],'Vline',[1])
+
+saveFigures = true;
+
+if saveFigures
+    set(gcf,'Position', [0, 0, 800, 800]);
+    set(gcf, 'Color', 'w');
+    export_fig(['./figures/point_analysis/loglog-PopulationVsAmenity.pdf']);
+end
